0f40763e435f8381e9cb0254693371779bbac79e
Molnar Emese <mse212002@gmail.com>
2020-04-21 12:46:03 +0300

    :sparkles: added docs controller

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,31 @@
+class DocsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def show
+    end
+
+    def create
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+      def find_doc
+      end
+
+      def doc_params
+      end
+end
