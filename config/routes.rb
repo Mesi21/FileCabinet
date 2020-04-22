580f3afd50d3f8103f9806ddd289aed5ed5bbe09
Mesi21 <mse212002@gmail.com>
2020-04-22 20:58:13 +0300

    :art: change route of app for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "welcome#index"
-
   resources :docs
+  authenticated :user do
+    root 'docs#index', as: 'authenticated_root'
+  end
+
+  root 'welcome#index'
 end
